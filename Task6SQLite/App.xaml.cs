46385a6bbcc8db25e85e3a98bb4bdd077761ce85
Derz65 <lm1030921@gmail.com>
2025-04-21 22:22:04 +0300

    main

---
+++ Task6SQLite/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+using Task6.Model;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Task6;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        DatabaseFacade facade = new DatabaseFacade(new ModelContext());
+        facade.EnsureCreated();
+    }
+}
+
